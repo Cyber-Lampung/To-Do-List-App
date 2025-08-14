ffff0a16f3083b13d48fa839ec30beb34c8ecfb8
Rado Hidayatulloh <acountgithub20@gmail.com>
2025-08-14 16:06:59 +0700

    add: feature remove items

--- To-Do-List-App/DeleteItems/Delete.js
+++ To-Do-List-App/DeleteItems/Delete.js
@@ -3,24 +3,43 @@ const IconDel = document.querySelectorAll("#trash");
 
 function checks() {
   IconDel.forEach((Del, index) => {
+    // debug awal => untuk mendapatkan index list
     //   console.log(index);
     Del.addEventListener("click", () => {
       const delDataList = confirm("Apakah kamu yakin ingin mengapus ini ? ");
 
       if (delDataList) {
         listElement.forEach((items, delIndex) => {
-          const found = index === delIndex;
-          const get = localStorage.getItem("list");
-          const check = get ? JSON.parse(get) : [];
+          // debug untuk mendapatkan data element dari element website
+          // console.log(items);
 
-          if (found) {
-            delete check[delIndex];
-            localStorage.setItem("list", JSON.stringify(check));
-            localStorage.removeItem("list");
-            window.location.href = "";
-          }
+          // debug untuk mendapatkan data setiap index data di local
+
+          const found = index === delIndex;
+
+          // debug found untuk mencocokkan index local dengan local Array jika ada maka true
+
+          // console.log(found);
+
+          const get = localStorage.getItem("list");
+          // debug get apakah dia mendapatkan data dari localStorage =>
+
+          // console.log(get); // / output => [{"Judul":"input","Description":"inputusername"}]
 
           // pecah check menjadi data asli
+
+          const check = get ? JSON.parse(get) : [];
+
+          // debug check apakah ada dan jika ada maka ubah ke data aslinya
+
+          // console.log("check length : ", check.length); // => output => Array [{"Judul":"input","Description":"inputusername"}]
+
+          if (check[index]) {
+            check.splice(index);
+            console.log(check);
+            localStorage.setItem("list", JSON.stringify(check));
+            location.reload();
+          }
         });
       }
     });
